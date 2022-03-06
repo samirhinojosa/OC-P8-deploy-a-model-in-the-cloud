9dce145a31dd994dfc420ba75596c38c1a8f8aab
Samir Hinojosa <samirhinojosa@gmail.com>
2022-03-06 08:32:38 +0000

    initializing PCA

--- test.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-import numpy as np
-
-c = np.array([[3.,4], [5.,6], [6.,7]])
-step = tf.reduce_mean(c, 1)                                                                                 
-with tf.Session() as sess:
-    print(sess.run(step))
